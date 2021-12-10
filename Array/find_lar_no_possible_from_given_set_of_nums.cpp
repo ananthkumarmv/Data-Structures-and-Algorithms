8f713d43d8b072a55771e731e786bd542feb23f8
Ananth <mvananthkumar@gmail.com>
2021-12-10 23:47:17 +0530

    Create find_lar_no_possible_from_given_set_of_nums.cpp

---
+++ Array/find_lar_no_possible_from_given_set_of_nums.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+//struct{
+//	bool operator()(int a, int b)
+//};
+
+
+bool customCompare(int a, int b){
+	string val1 = to_string(a) + to_string(b);
+	string val2 = to_string(b) + to_string(a);
+	
+	return val1>val2;
+}
+
+string findLargestNumber(vector<int> &nums){
+	sort(begin(nums), end(nums), customCompare);
+	string s;
+	for(int &i: nums){
+		s +=to_string(i);
+	}
+	
+	return s;
+}
+
+
+int main(){
+	
+	vector<int> nums = { 10, 68, 97, 9, 21, 12 };
+	
+	cout<<"The largest number is: "<<findLargestNumber(nums)<<endl;
+
+
+    return 0;
+}
+
+
