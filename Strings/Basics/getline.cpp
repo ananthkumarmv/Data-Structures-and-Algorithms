089c003691418597d0a3002f6533e385bcdbe6e1
Ananth <mvananthkumar@gmail.com>
2022-03-04 17:26:57 +0530

    Update getline.cpp

--- Strings/Basics/getline.cpp
+++ Strings/Basics/getline.cpp
@@ -5,15 +5,8 @@ using namespace std;
 int main(){
 	
 	string str;
-	getline(cin, str);
+	getline(cin, str);  //also reads space
 	cout<<str.length();
-	int i=0,special=0;
-	while(str[i] != '\0') 
-    { 
-    	if((isalpha(str[i])==0) && (isdigit(str[i])==0) && (isspace(str[i])==0)) special++;    	 
-    	i++; 
-	} 
-	cout<<special;
     return 0;
 }
 
