24257e3af070ce250970f08c7f4fa5723e37a187
Ananth <mvananthkumar@gmail.com>
2022-03-04 15:19:51 +0530

    Create vector-of-vectors.cpp

---
+++ Array/Basics/vector-of-vectors.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void print(vector< vector<int>> &arr){
+	for(int i=0; i<arr.size();i++){
+		for(int j=0; j<arr[i].size(); j++){
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+}
+
+
+int main(){
+	int m=3, n=2;
+	vector<vector<int>> arr;
+	for(int i=0; i<m; i++){
+		vector<int> v;
+		for(int j=0; j<n; j++)
+			v.push_back(i);
+		arr.push_back(v);
+	}
+	
+	print(arr);
+
+    return 0;
+}
+
+
