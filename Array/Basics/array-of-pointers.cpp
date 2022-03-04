739bc0b9ae039828811e4c5d4973a32fcff5a7c0
Ananth <mvananthkumar@gmail.com>
2022-03-04 12:21:20 +0530

    Create array-of-pointers.cpp

---
+++ Array/Basics/array-of-pointers.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	int m=3, n=2;
+	int *arr[m];
+	
+	for(int i=0; i<m;i++){
+		arr[i]=new int[n];
+		for(int j=0; j<n;j++){
+			arr[i][j]=i;
+			cout<<arr[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	
+    return 0;
+}
+
+
