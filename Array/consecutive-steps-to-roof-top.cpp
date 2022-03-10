3b357cb479115c467b3fae0a616584420bc67de4
Ananth <mvananthkumar@gmail.com>
2022-03-10 22:32:55 +0530

    Update consecutive-steps-to-roof-top.cpp

--- Array/consecutive-steps-to-roof-top.cpp
+++ Array/consecutive-steps-to-roof-top.cpp
@@ -1,3 +1,11 @@
+//Input : arr[] = {1, 2, 2, 3, 2}
+//Output : 1
+//Explanation :
+//Maximum consecutive steps from 1 to 2 OR 2 to 3.
+//Input : arr[] = {1, 2, 3, 4}
+//Output : 3
+
+
 #include<iostream>
 
 using namespace std;
