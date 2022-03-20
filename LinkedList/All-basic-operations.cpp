d3ef1dae8c5e1bf1f29286eb26622e33214ed099
Ananth <mvananthkumar@gmail.com>
2022-03-20 20:24:32 +0530

    Update All-basic-operations.cpp

--- LinkedList/All-basic-operations.cpp
+++ LinkedList/All-basic-operations.cpp
@@ -2,10 +2,33 @@
 
 using namespace std;
 
-struct Node{
-	int data;
-	Node* next;
+class Node{
+	public:
+		int data;
+		Node* next;
 };
+
+// Insert
+void insertPos(Node** root, int x, int pos){
+	Node* ins = new Node;
+	ins->data = x;
+	if(pos == 1){
+		ins->next = *root;
+		*root = ins;
+		return;
+	}
+	
+	Node* temp = *root;
+	for(int i=1; i<=pos-2 && temp!=NULL; i++)
+		temp = temp->next;
+	
+	if(temp==NULL)
+		return;
+		
+	ins->next = temp->next;
+	temp->next  = ins;
+	
+}
 
 
 void insertBeg(Node** root, int x){
@@ -39,7 +62,7 @@ Node* insert(int arr[], int n){
 	return root;
 }
 
-
+// Delete
 Node* deleteHeadNode(Node** root){
 	Node* temp = *root;
 	if(temp == NULL) 
@@ -93,6 +116,25 @@ void deleteNode(Node** root, int key){
 	
 }
 
+// Search
+
+int search(Node** root, int x){
+	Node* temp = *root;
+	int pos=1;
+	
+	while(temp!=NULL){
+		if(temp->data == x)
+			return pos;
+		else{
+			pos++;
+			temp= temp->next;
+		}
+	}
+	
+	return -1;
+}
+
+
 void display(Node* root){
 	while(root!=NULL){
 		cout<<root->data<<" ";
@@ -120,7 +162,13 @@ int main(){
 	deleteLastNode(&root);
 	display(root);
 	
-
+	insertPos(&root, 5, 1);
+	display(root);
+	
+	int x = search(&root, 3);
+	x ==-1 ? cout<<"Not found" : cout<<x;
+	
+	
     return 0;
 }
 
