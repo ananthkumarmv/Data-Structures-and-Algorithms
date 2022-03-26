a6e201e7ad1bd3de3ff62ddafda06353f6ea061b
Ananth <mvananthkumar@gmail.com>
2022-03-26 22:20:46 +0530

    Create KStack-in-an-array.cpp

---
+++ Stack/KStack-in-an-array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+struct KStacks{
+	int *arr, *top, *next;
+	int k, freeTop, cap;
+	KStacks(int k1, int n){
+		k=k1, cap=n;
+		
+	}
+};
+
+
+
+int main(){
+
+
+
+    return 0;
+}
+
+
