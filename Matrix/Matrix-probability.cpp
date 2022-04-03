f83d1fd531f7dd6079daa71108413a82cf0a3e6e
Ananth <mvananthkumar@gmail.com>
2022-04-03 23:31:35 +0530

    Create Matrix-probability.cpp

---
+++ Matrix/Matrix-probability.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+  
+
+bool isSafe(int x, int y, int m, int n)
+{
+    return (x >= 0 && x < m && 
+            y >= 0 && y < n);
+}
+
+double findProbability(int m, int n, int x, 
+                       int y, int N)
+{
+
+    if (!isSafe(x, y, m, n))
+        return 0.0;
+  
+    if (N == 0)
+        return 1.0;
+  
+    double prob = 0.0;
+  
+    prob += findProbability(m, n, x - 1, 
+                            y, N - 1) * 0.25;
+  
+    prob += findProbability(m, n, x, 
+                            y + 1, N - 1) * 0.25;
+  
+    prob += findProbability(m, n, x + 1,
+                            y, N - 1) * 0.25;
+  
+    prob += findProbability(m, n, x, 
+                            y - 1, N - 1) * 0.25;
+  
+    return prob;
+}
+  
+
+int main()
+{
+    int m = 5, n = 5;
+  
+    int i = 1, j = 1;
+  
+    int N = 2;
+    cout << "Probability is "
+        << findProbability(m, n, i, j, N);
+  
+    return 0;
+}
