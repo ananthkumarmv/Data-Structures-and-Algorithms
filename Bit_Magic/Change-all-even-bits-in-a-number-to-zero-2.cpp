2b7ef8a046840b26cb299cf03358e5747df56788
Ananth <mvananthkumar@gmail.com>
2022-04-06 15:45:47 +0530

    Create Change-all-even-bits-in-a-number-to-zero-2.cpp

---
+++ Bit_Magic/Change-all-even-bits-in-a-number-to-zero-2.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int changeEvenBitToOne(int n){
+	
+	int a=1;
+    for(int i=0;i<31;i=i+2){
+        n=n&(~(a<<i));
+    }
+    return n;
+	
+}
+
+int main(){
+	
+	int n;
+	cin>>n;
+	cout<<changeEvenBitToOne(n);
+
+    return 0;
+}
+
