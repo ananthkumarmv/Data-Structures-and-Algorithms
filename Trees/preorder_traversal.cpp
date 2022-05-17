34caae5995a6b2624cd589b3d4bcc752b30a9482
Ananth <mvananthkumar@gmail.com>
2022-05-17 11:07:43 +0530

    Update preorder_traversal.cpp

--- Trees/preorder_traversal.cpp
+++ Trees/preorder_traversal.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+class Node{
+	public:
+		int data;
+		Node *left, *right;
+	
+	Node(int val){
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+	
+};
+
+
 void preorder(Node *root){
 	if(root!=NULL){
-		cout<<(root->key)<<" ";
+		cout<<(root->data)<<" ";
 		preorder(root->left);
 		preorder(root->right);
 	}
@@ -12,8 +26,15 @@ void preorder(Node *root){
 
 
 int main(){
-
-
+	
+	Node* root = new Node(1);
+	
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(4);
+	root->left->right = new Node(5);
+	
+	preorder(root);
 
     return 0;
 }
