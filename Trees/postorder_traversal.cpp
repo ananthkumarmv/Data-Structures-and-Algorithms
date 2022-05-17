1afea0e3cec7465d0cc4456e9d0aa700e6d5f9a7
Ananth <mvananthkumar@gmail.com>
2022-05-17 11:07:40 +0530

    Update postorder_traversal.cpp

--- Trees/postorder_traversal.cpp
+++ Trees/postorder_traversal.cpp
@@ -2,18 +2,39 @@
 
 using namespace std;
 
+class Node{
+	public:
+		int data;
+		Node *left, *right;
+	
+	Node(int val){
+		data = val;
+		left = NULL;
+		right = NULL;
+	}
+	
+};
+
 
 void postorder(Node *root){
 	if(root!=NULL){
 		postorder(root->left);
 		postorder(root->right);
-		cout<<(root->key)<<" ";
+		cout<<(root->data)<<" ";
 	}
 }
 
 
 int main(){
-
+	
+	Node* root = new Node(1);
+	
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(4);
+	root->left->right = new Node(5);
+	
+	postorder(root);
 
 
     return 0;
