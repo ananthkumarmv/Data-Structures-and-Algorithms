1ad3faa1b7918d765fccbe4999e0ce0739d7ec44
Ananth <mvananthkumar@gmail.com>
2022-05-31 22:28:37 +0530

    Update Binary_search.cpp

--- All_searching_algorithm/Binary_search.cpp
+++ All_searching_algorithm/Binary_search.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 void binary_search(int a[], int n, int k){
 	int low=0, high=n-1;
-	int mid=(low+high)/2;
+	int mid= low + (high - low)/2;
 	while(low<=high){
-		mid = (low+high)/2;
+		mid = low + (high - low)/2;
 		if(a[mid]==k){
 			cout<<"Element "<<k<<" found at position "<<mid+1;
 			return;
